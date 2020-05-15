a8980d4d62c0556b82eddb76cf60a9f5326b7bf1
dmentdc <seba.ariascandia@gmail.com>
2020-05-14 21:13:50 -0400

    correción a ejercicio mayor de tres

--- clase_4/mayor_de_tres.py
+++ clase_4/mayor_de_tres.py
@@ -1,24 +1,30 @@
 import sys
 
+# primero, si los argumentos no son los necesarios, el programa no sigue.
 # Obtener el numero mayor de los numeros ingresados entre tres
 #comparamos el primero con el segundo, 
 #comparamos el primero con el tercero
 #condicion de borde, si son iguales todos o entre dos.
-numeroUno = int(sys.argv[1])
-numeroDos = int(sys.argv[2])
-numeroTres = int(sys.argv[3])
-mayor = 0
+if(len(sys.argv) == 4):
+    numeroUno = int(sys.argv[1])
+    numeroDos = int(sys.argv[2])
+    numeroTres = int(sys.argv[3])
+    mayor = 0
 
-if(numeroUno == numeroDos == numeroTres):
-    print("no hay mayores")
-else:
-    if(numeroUno > numeroDos):
-        mayor = numeroUno
-    elif(numeroUno > numeroTres):
-        mayor = numeroUno
-    elif(numeroDos > numeroTres):
-        mayor = numeroDos
+    if(numeroUno == numeroDos == numeroTres):
+        print("no hay mayores")
     else:
-        mayor = numeroTres
-    print("el numero mayor de todos es: {}".format(str(mayor)))
+        if(numeroUno > numeroDos):
+            mayor = numeroUno
+        elif(numeroUno > numeroTres):
+            mayor = numeroUno
+        elif(numeroDos > numeroTres):
+            mayor = numeroDos
+        else:
+            mayor = numeroTres
+        print("el numero mayor de todos es: {}".format(str(mayor)))
+else:
+    print("sonamos, hay un problema con los argumentos ingresados")
+
+
 
